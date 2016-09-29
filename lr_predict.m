2cf4215c81833a48dbb15ec4d13fbbb527087f0f
XIN Hao <hxinaa@cse.ust.hk>
2016-09-29 16:17:16 +0800

    delete files

--- lr_predict.m
+++
@@ -1,9 +0,0 @@
-%% logistic regression predict
-% input X, test data, n*m, n items, m features
-%       w, logistic regression model, m*1
-% output y, predict value
-function y = lr_predict(X,w)
-    [n,m] = size(X);
-    X = [X,ones(n,1)];
-    y = sigmoid(X*w)>(0.5.*ones(n,1)); 
-end
--- sigmoid.m
+++
@@ -1,6 +0,0 @@
-%% sigmod function
-% input x, n*1, n items,
-% output sigmod(X)
-function o = sigmoid(x)
-    o = 1./(1.+exp(-x));
-end
